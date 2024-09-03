0aa40154279abf65feccb204082294d3ffdacbce
mj <yun0602@daum.net>
2024-09-03 16:29:53 +0900

    수정

--- common/js/script.ui.js
+++ common/js/script.ui.js
@@ -5,7 +5,21 @@ $(window).scroll(function(){
       $('header').removeClass('active')
   };
 });
+
 $(function(){
+  // GNB
+  $( '.header-nav > li > span' ).each(function() {
+    $(this).mouseenter(function () {
+      $(this).addClass('gnb-active');
+      $(this).parent().addClass('gnb-active');
+    });
+    $('.header-nav li').mouseleave(function () {
+      //alert(11);
+        $('.header-nav > li > span').removeClass('gnb-active');
+        $(this).removeClass('gnb-active');
+      });
+  });
+
   // visual
   var swiper = new Swiper(".main-visual-sec .swiper-container", {
     loop : true,
@@ -22,5 +36,23 @@ $(function(){
       el: ".main-visual-sec .swiper-pagination",
       clickable: true
     },
-  });  
+  });
+
+  //FAQ
+	$('.faq-a').hide();
+	$('.folder-list > li > .faq-q').on('click', function(e) {
+		e.preventDefault();
+		if ($(this).parent('li').hasClass('open')) {
+			$(this).parent('li').removeClass('open').addClass('close');
+			$(this).next('.faq-a').hide();
+		} else {
+			$(this).parent('li').removeClass('close').addClass('open');
+			$(this).siblings('.folder-list > li > .faq-q').removeClass('open').addClass('close');
+			$(this).siblings('.faq-a').hide();
+			$(this).next('.faq-a').show();
+		}
+	});
+  
+  
+
 });
